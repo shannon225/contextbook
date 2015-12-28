a28bb599b101d5e823cbeac49e85d970a22c1677
searleb <searleb@uw.edu>
2015-12-27 18:50:45 -0800

    cleaned up unit test

--- src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/pecan/PecanRawScorerTest.java
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/pecan/PecanRawScorerTest.java
@@ -11,15 +11,14 @@ public class PecanRawScorerTest extends TestCase {
 	public void testGetIndividualPeakScores() {
 		PecanRawScorer scorer=new PecanRawScorer(new MassTolerance(10.0f), null);
 		LibraryEntry entry=getEntry(new double[] {300.0, 1000.01, 1200.0}, new float[] {7, 10, 4});
-		Stripe spectrum=getStripe(new double[] {1000.0, 1000.01, 1000.02, 1000.03, 1000.04, 1000.05}, new float[] {10.0f, 100.0f, 50.0f, 10.0f, 1.0f, 100f});
+		Stripe spectrum=getStripe(new double[] {1000.0, 1000.011, 1000.02, 1000.03, 1000.04, 1000.05}, new float[] {10.0f, 100.0f, 50.0f, 10.0f, 1.0f, 100f});
 		PeakScores[] scores=scorer.getIndividualPeakScores(entry, spectrum, false);
 		
 		assertEquals(entry.getMassArray().length, scores.length);
-		for (int i=0; i<scores.length; i++) {
-			if (scores[i]!=null) {
-				System.out.println(i+") "+entry.getMassArray()[i]+"\t"+scores[i].getScore()+"\t"+scores[i].getDeltaMass());
-			}
-		}
+		assertNull(scores[0]);
+		assertEquals(scores[1].getScore(), 1600.0, 0.1);
+		assertEquals(scores[1].getDeltaMass(), -1, 0.001);
+		assertNull(scores[2]);
 	}
 	
 	public LibraryEntry getEntry(double[] masses, float[] intensities) {
