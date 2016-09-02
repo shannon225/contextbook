43e723ac9e8e71dd779b5a74b2afa95e632ba303
searleb <searleb@uw.edu>
2016-09-02 15:48:47 -0700

    fixed array index error, updated to 0.2.8

--- src/main/java/edu/washington/gs/maccoss/encyclopedia/algorithms/alignment/RetentionTimeFilter.java
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/algorithms/alignment/RetentionTimeFilter.java
@@ -201,8 +201,10 @@ public class RetentionTimeFilter {
 		Arrays.sort(deltaArray);
 		
 		// assumes 1% FDR
-		min=deltaArray[Math.round(deltaArray.length*0.005f)];
-		max=deltaArray[Math.round(deltaArray.length*0.995f)];
+		int lowerIndex=Math.max(Math.round(deltaArray.length*0.005f), deltaArray.length-1);
+		min=deltaArray[lowerIndex];
+		int upperIndex=Math.round(deltaArray.length*0.995f);
+		max=deltaArray[Math.max(upperIndex, deltaArray.length-1)];
 		
 		//Arrays.sort(deltaArray);
 		//min=deltaArray[Math.round(deltaArray.length*0.05f)];
