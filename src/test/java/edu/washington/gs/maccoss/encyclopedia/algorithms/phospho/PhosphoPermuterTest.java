49478749116129ead38d1a14bd5041815a322bb7
searleb <searleb@uw.edu>
2016-10-31 20:47:03 -0700

    added todo, added test

--- src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/phospho/PhosphoPermuterTest.java
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/phospho/PhosphoPermuterTest.java
@@ -2,8 +2,10 @@ package edu.washington.gs.maccoss.encyclopedia.algorithms.phospho;
 
 import java.util.ArrayList;
 
+import edu.washington.gs.maccoss.encyclopedia.datastructures.FragmentationModel;
 import edu.washington.gs.maccoss.encyclopedia.datastructures.SearchParameters;
 import edu.washington.gs.maccoss.encyclopedia.filereaders.SearchParameterParser;
+import edu.washington.gs.maccoss.encyclopedia.utils.massspec.FragmentIon;
 import junit.framework.TestCase;
 
 public class PhosphoPermuterTest extends TestCase {
@@ -41,6 +43,76 @@ public class PhosphoPermuterTest extends TestCase {
 		assertEquals(expected.length, permutations.size());
 		for (int i=0; i<permutations.size(); i++) {
 			assertEquals(expected[i], permutations.get(i));
+		}
+	}
+	
+	public void testFragmentation() {
+		SearchParameters parameters=SearchParameterParser.getDefaultParametersObject();
+		ArrayList<String> permutations=PhosphoPermuter.getPermutations("SGS[+80]VSNYR", parameters.getAAConstants());
+		double[][] expected=new double[][] {new double[] {
+				// S[+79.96633]GSVSNYR
+				175.11895217, //y1
+				338.18227217000003, //y2
+				452.22519917000005, //y3
+				539.2572271700001, //y4
+				638.32564117, //y5
+				725.35766917, //y6
+				782.37913317, //y7
+				851.40059517, //y8-NL
+				949.37749117, //y8
+			}, new double[] {
+				// SGS[+79.96633]VSNYR
+				175.11895217, //y1
+				338.18227217000003, //y2
+				452.22519917000005, //y3
+				539.2572271700001, //y4
+				638.32564117, //y5
+				707.34710317, //y6-NL
+				764.36856717, //y7-NL
+				805.32399917, //y6
+				851.40059517, //y8-NL
+				862.34546317, //y7
+				949.37749117, //y8
+			}, new double[] {
+				// SGSVS[+79.96633]NYR
+				175.11895217, //y1
+				338.18227217000003, //y2
+				452.22519917000005, //y3
+				521.24666117, //y4-NL
+				619.22355717, //y4
+				620.31507517, //y5-NL
+				707.34710317, //y6-NL
+				718.29197117, //y5
+				764.36856717, //y7-NL
+				805.32399917, //y6
+				851.40059517, //y8-NL
+				862.34546317, //y7
+				949.37749117, //y8
+			}, new double[] {
+				// SGSVSNY[+79.96633]R
+				175.11895217, //y1
+				320.17170617, //y2-NL
+				418.14860217, //y2
+				434.21463317, //y3-NL
+				521.24666117, //y4-NL
+				532.19152917, //y3
+				619.2235571699999, //y4
+				620.31507517, //y5-NL
+				707.34710317, //y6-NL
+				718.2919711699999, //y5
+				764.36856717, //y7-NL
+				805.3239991699999, //y6
+				851.40059517, //y8-NL
+				862.3454631699999, //y7
+				949.3774911699999, //y8
+		}};
+
+		for (int i=0; i<permutations.size(); i++) {
+			FragmentationModel model=new FragmentationModel(permutations.get(i), parameters.getAAConstants());
+			FragmentIon[] yions=model.getYIons();
+			for (int j=0; j<yions.length; j++) {
+				assertEquals(expected[i][j], yions[j].mass, 0.01);
+			}
 		}
 	}
 }
