cb127bb830bc0350e2c58a70257339ae3eff2b5e
searleb <searleb@uw.edu>
2017-04-24 13:18:36 -0700

    added mzml structure plotter

---
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/gui/dia/MzmlStructureCharter.java
@@ -0,0 +1,101 @@
+package edu.washington.gs.maccoss.encyclopedia.gui.dia;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.jfree.chart.ChartPanel;
+
+import edu.washington.gs.maccoss.encyclopedia.datastructures.Range;
+import edu.washington.gs.maccoss.encyclopedia.datastructures.SearchParameters;
+import edu.washington.gs.maccoss.encyclopedia.filereaders.MzmlScanRangeTrackerSAXProducer;
+import edu.washington.gs.maccoss.encyclopedia.filereaders.PecanParameterParser;
+import edu.washington.gs.maccoss.encyclopedia.filereaders.StripeFile;
+import edu.washington.gs.maccoss.encyclopedia.gui.general.Charter;
+import edu.washington.gs.maccoss.encyclopedia.utils.EncyclopediaException;
+import edu.washington.gs.maccoss.encyclopedia.utils.Logger;
+import edu.washington.gs.maccoss.encyclopedia.utils.graphing.GraphType;
+import edu.washington.gs.maccoss.encyclopedia.utils.graphing.XYTrace;
+import edu.washington.gs.maccoss.encyclopedia.utils.graphing.XYTraceInterface;
+import gnu.trove.list.array.TFloatArrayList;
+
+public class MzmlStructureCharter {
+
+	public static ChartPanel getStructureChart(File mzMLFile) {
+		HashMap<String, String> paramMap=PecanParameterParser.getDefaultParameters();
+		paramMap.put("-acquisition", "DIA"); // NON-OVERLAPPING!
+		SearchParameters parameters=PecanParameterParser.parseParameters(paramMap);
+
+		HashMap<Range, TFloatArrayList> retentionTimesByStripe=null;
+		HashMap<Range, TFloatArrayList> retentionTimesByPrecursor=null;
+		try {
+			Logger.logLine("Indexing "+mzMLFile.getName()+" ...");
+			StripeFile stripeFile=new StripeFile();
+			stripeFile.openFile();
+
+			MzmlScanRangeTrackerSAXProducer producer=new MzmlScanRangeTrackerSAXProducer(mzMLFile, parameters);
+			retentionTimesByStripe=producer.getRetentionTimesByStripe().getStripeRTsInSecs();
+			retentionTimesByPrecursor=producer.getRetentionTimesByStripe().getPrecursorRTsInSecs();
+
+			Thread producerThread=new Thread(producer);
+
+			Thread[] threads=new Thread[] {producerThread};
+
+			for (int i=0; i<threads.length; i++) {
+				threads[i].start();
+			}
+
+			try {
+				for (int i=0; i<threads.length; i++) {
+					threads[i].join();
+				}
+			} catch (InterruptedException ie) {
+				Logger.errorLine("DIA reading interrupted!");
+				Logger.errorException(ie);
+			}
+		} catch (IOException ioe) {
+			throw new EncyclopediaException("DIA reading IO error!", ioe);
+		} catch (SQLException sqle) {
+			throw new EncyclopediaException("DIA reading SQL error!", sqle);
+		}
+
+		float firstScan=Float.MAX_VALUE;
+		ArrayList<XYTraceInterface> traces=new ArrayList<>();
+		for (Entry<Range, TFloatArrayList> entry : retentionTimesByStripe.entrySet()) {
+			Range range=entry.getKey();
+			TFloatArrayList rts=entry.getValue();
+			if (rts.size()>0) {
+				if (rts.get(0)<firstScan) firstScan=rts.get(0);
+
+				XYTraceInterface trace=new XYTrace(new float[] {range.getStart(), range.getStop()}, new float[] {rts.get(0), rts.get(0)}, GraphType.squaredline, range.toString(),
+						new Color(100, 100, 255), 5.0f);
+				traces.add(trace);
+				if (rts.size()>1) {
+					trace=new XYTrace(new float[] {range.getStart(), range.getStop()}, new float[] {rts.get(1), rts.get(1)}, GraphType.squaredline, range.toString(), new Color(0, 0, 200), 5.0f);
+					traces.add(trace);
+					trace=new XYTrace(new float[] {range.getStop(), range.getStop()}, new float[] {rts.get(0), rts.get(1)}, GraphType.dashedline, range.toString(), Color.gray, 1.0f);
+					traces.add(trace);
+				}
+			}
+		}
+
+		for (Entry<Range, TFloatArrayList> entry : retentionTimesByPrecursor.entrySet()) {
+			Range range=entry.getKey();
+			TFloatArrayList rts=entry.getValue();
+			for (float rt : rts.toArray()) {
+				if (rt>firstScan) {
+					XYTraceInterface trace=new XYTrace(new float[] {range.getStart(), range.getStop()}, new float[] {rt, rt}, GraphType.squaredline, range.toString(), Color.LIGHT_GRAY, 5.0f);
+					traces.add(trace);
+				}
+			}
+		}
+
+		ChartPanel panel=Charter.getChart("M/Z ("+mzMLFile.getName()+")", "Retention Time (secs)", false, traces.toArray(new XYTraceInterface[traces.size()]));
+		return panel;
+	}
+
+}
---
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/gui/dia/MzmlStructureCharterTest.java
@@ -0,0 +1,30 @@
+package edu.washington.gs.maccoss.encyclopedia.gui.dia;
+
+import java.awt.Dimension;
+import java.io.File;
+
+import org.jfree.chart.ChartPanel;
+
+import edu.washington.gs.maccoss.encyclopedia.gui.dia.MzmlStructureCharter;
+import edu.washington.gs.maccoss.encyclopedia.gui.general.Charter;
+
+public class MzmlStructureCharterTest {
+
+	public static void main(String[] args) {
+		File mzMLFile=new File("/Users/searleb/Documents/school/projects/pecandata/121115_BCS_HeLa_24mz_400_1000.mzML");
+		mzMLFile=new File("/Users/searleb/Documents/projects/encyclopedia/mzml/test/fast/121115_BCS_HeLa_24mz_400_1000.mzML");
+		mzMLFile=new File("/Volumes/DataBackup/david_hawke/040117HeLa-dig-2ug-longDIA.mzML");
+		// mzMLFile=new
+		// File("/Volumes/DataBackup/david_hawke/040417HeLa-dig-2ug-longDIA.mzML");
+		// mzMLFile=new
+		// File("/Volumes/DataBackup/david_hawke/040117HeLa-dig-2ug-longerDIA.mzML");
+		// mzMLFile=new
+		// File("/Volumes/DataBackup/david_hawke/040417HeLa-dig-2ug-longDIA_2.mzML");
+		// mzMLFile=new
+		// File("/Volumes/DataBackup/david_hawke/040317HeLa-dig-2ug-longDIAthermo.mzML");
+		mzMLFile=new File("/Users/searleb/Downloads/2017_January_23_envtstress_geoduck3.mzML");
+
+		ChartPanel panel=MzmlStructureCharter.getStructureChart(mzMLFile);
+		Charter.launchComponent(panel, mzMLFile.getName(), new Dimension(792, 612));
+	}
+}
