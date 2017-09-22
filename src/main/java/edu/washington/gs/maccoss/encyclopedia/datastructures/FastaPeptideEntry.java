705f00ba83f9fd879922221a0e8b891ba0dcb0f3
Seth Just <seth.just@proteomesoftware.com>
2017-09-22 13:36:57 -0700

    Issue #62. Write unit test and modify comparison method to ensure that lexicographic ordering by (sorted) accessions is maintained.
    --HG--
    branch : issue-62

--- src/main/java/edu/washington/gs/maccoss/encyclopedia/datastructures/FastaPeptideEntry.java
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/datastructures/FastaPeptideEntry.java
@@ -39,19 +39,21 @@ public class FastaPeptideEntry implements Comparable<FastaPeptideEntry> {
 	@Override
 	public int compareTo(FastaPeptideEntry o) {
 		if (o==null) return 1;
-		
-		int c = getAccessions().size() - o.getAccessions().size();
-		if (c!=0) return c;
+
+		int c;
 
 		final Iterator<String>
 				iter = ImmutableSortedSet.copyOf(getAccessions()).iterator(),
 				oIter = ImmutableSortedSet.copyOf(o.getAccessions()).iterator();
-		while (iter.hasNext()) {
+		while (iter.hasNext() && oIter.hasNext()) {
 			String acc = iter.next(), oAcc = oIter.next();
 
 			c = acc.compareTo(oAcc);
 			if (c!=0) return c;
 		}
+
+		c = getAccessions().size() - o.getAccessions().size();
+		if (c!=0) return c;
 
 		c=filename.compareTo(o.getFilename());
 		if (c!=0) return c;
