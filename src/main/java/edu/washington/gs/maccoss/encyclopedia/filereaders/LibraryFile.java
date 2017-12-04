ae56917e48392f55a3b9130c13a274113c3f3cdd
Caleb Emmons <caleb.emmons@proteomesoftware.com>
2017-12-04 09:56:21 -0800

    Restore constants in LibraryFile.
    --HG--
    branch : chrom-store

--- src/main/java/edu/washington/gs/maccoss/encyclopedia/filereaders/LibraryFile.java
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/filereaders/LibraryFile.java
@@ -67,6 +67,9 @@ public class LibraryFile extends SQLFile implements LibraryInterface {
 	private static final String SOURCE_FILE_SPLIT="|";
 	public static final String DLIB=".dlib";
 	public static final String ELIB=".elib";
+	public static final String ENCYCLOPEDIA_VERSION = "EncyclopediaVersion";
+	public static final String PERCOLATOR_VERSION = "PercolatorVersion";
+	public static final String UNKNOWN = "Unknown";
 	public static final Version[] ACCEPTABLE_VERSIONS=new Version[] { new Version(0, 1, 0), new Version(0, 1, 1), new Version(0, 1, 2), new Version(0, 1, 3), new Version(0, 1, 4),
 			new Version(0, 1, 5), new Version(0, 1, 6), new Version(0, 1, 7), new Version(0, 1, 8), new Version(0, 1, 9), new Version(0, 1, 10), new Version(0, 1, 11), new Version(0, 1, 12), new Version(0, 1, 13) };
 	public static final Version MOST_RECENT_VERSION=new Version(0, 1, 13);
