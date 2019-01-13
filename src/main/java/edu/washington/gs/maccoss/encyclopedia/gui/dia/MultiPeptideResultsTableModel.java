f8a31c27e9973421c991c36f2a9b6b8d344d19af
searleb <none@none>
2019-01-13 23:56:23 +0100

    Update to version for release encyclopedia-0.8.1.

--- src/main/java/edu/washington/gs/maccoss/encyclopedia/gui/dia/MultiPeptideResultsTableModel.java
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/gui/dia/MultiPeptideResultsTableModel.java
@@ -4,12 +4,16 @@ import java.util.ArrayList;
 
 import javax.swing.table.AbstractTableModel;
 
+import edu.washington.gs.maccoss.encyclopedia.datastructures.AminoAcidConstants;
+import edu.washington.gs.maccoss.encyclopedia.datastructures.FragmentationModel;
 import edu.washington.gs.maccoss.encyclopedia.datastructures.PeptideReportData;
+import edu.washington.gs.maccoss.encyclopedia.utils.massspec.PeptideUtils;
 
 public class MultiPeptideResultsTableModel extends AbstractTableModel {
 	private static final long serialVersionUID=1L;
+	private final AminoAcidConstants aaConstants=new AminoAcidConstants();
 	
-	private final String[] columns=new String[] {"#", "Peptide", "Protein", "Quant Ions", "Average RT", "Charge"};
+	private final String[] columns=new String[] {"#", "Peptide", "Protein", "Quant Ions", "Average RT", "m/z", "Charge"};
 
 	ArrayList<PeptideReportData> allEntries=new ArrayList<PeptideReportData>();
 	ArrayList<PeptideReportData> entries=new ArrayList<PeptideReportData>();
@@ -61,7 +65,8 @@ public class MultiPeptideResultsTableModel extends AbstractTableModel {
 			case 2: return String.class;
 			case 3: return Integer.class;
 			case 4: return Float.class;
-			case 5: return Integer.class;
+			case 5: return Double.class;
+			case 6: return Integer.class;
 		}
 		return Object.class;
 	}
@@ -76,7 +81,11 @@ public class MultiPeptideResultsTableModel extends AbstractTableModel {
 			case 2: return entry.getAccessions();
 			case 3: return entry.getMaxNumOfFragments();
 			case 4: return entry.getAverageRetentionTime()/60f;
-			case 5: return entry.getPrecursorCharge();
+			case 5:
+				FragmentationModel model=PeptideUtils.getPeptideModel(entry.getPeptideModSeq(), aaConstants);
+				return model.getChargedMass(entry.getPrecursorCharge());
+				 
+			case 6: return entry.getPrecursorCharge();
 		}
 		return null;
 	}
