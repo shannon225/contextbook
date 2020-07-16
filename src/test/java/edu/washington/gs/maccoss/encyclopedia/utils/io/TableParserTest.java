1b977402ee510f9b25e19adce675deb88812151a
Seth Just <seth.just@proteomesoftware.com>
2020-07-16 13:48:59 -0700

    Update to JUnit 4, as this allows for much safer testing of multithreaded code by using test timeouts. All existing test code written against JUnit 3 continues to be fully supported, and JUnit3-style tests can easily be written.

--- src/test/java/edu/washington/gs/maccoss/encyclopedia/utils/io/TableParserTest.java
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/utils/io/TableParserTest.java
@@ -1,24 +1,33 @@
 package edu.washington.gs.maccoss.encyclopedia.utils.io;
 
-import junit.framework.TestCase;
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
-public class TableParserTest extends TestCase {
+import static org.junit.Assert.assertNotNull;
+
+public class TableParserTest {
+	private static final long TIMEOUT = 5000L;
+
 	private Path tmp;
 
+	@Before
 	public void setUp() throws Exception {
 		tmp = Files.createTempFile("test_", ".tsv");
 	}
 
+	@After
 	public void tearDown() throws Exception {
 		FileUtils.deleteQuietly(tmp.toFile());
 		tmp = null;
 	}
 
+	@Test(timeout = TIMEOUT)
 	public void testSimpleMuscle() throws Exception {
 		TableParser.parseTSV(
 				tmp.toFile(),
@@ -26,18 +35,22 @@ public class TableParserTest extends TestCase {
 		);
 	}
 
+	@Test(timeout = TIMEOUT)
 	public void testNoSuchFile() throws Exception {
 
 	}
 
+	@Test(timeout = TIMEOUT)
 	public void testEmptyFile() throws Exception {
 
 	}
 
+	@Test(timeout = TIMEOUT)
 	public void testErrorProducing() throws Exception {
 
 	}
 
+	@Test(timeout = TIMEOUT)
 	public void testErrorConsuming() throws Exception {
 
 	}
