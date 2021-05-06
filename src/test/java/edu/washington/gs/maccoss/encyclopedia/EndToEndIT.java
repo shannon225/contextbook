0eca7cf0978fc50ec024b214f09c936ca66aa699
Kevin Calaway <kevin.calaway@proteomesoftware.com>
2021-05-06 14:54:29 -0700

    Move tempReport also to setUp/tearDown

--- src/test/java/edu/washington/gs/maccoss/encyclopedia/EndToEndIT.java
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/EndToEndIT.java
@@ -45,6 +45,9 @@ public class EndToEndIT {
 	File fastaFile;
 	File libraryFile;
 	LibraryFile libraryInterface;
+
+	File tempReport;
+
 	LibraryScoringFactory libraryScoringFactory;
 	SearchParameters parameters;
 	Path tempDir;
@@ -78,6 +81,9 @@ public class EndToEndIT {
 		diaFile3 = getResourceAsTempFile(getClass(), "/edu/washington/gs/maccoss/encyclopedia/testdata/121115_bcs_hela_24mz_400_1000_0D_3_600.dia", tempDir, "EndToEnd", ".dia").toFile();
 		fastaFile = getResourceAsTempFile(getClass(), "/edu/washington/gs/maccoss/encyclopedia/testdata/uniprot_human_2018.subset.fasta", tempDir, name, ".fasta").toFile();
 		libraryScoringFactory = new EncyclopediaOneScoringFactory(parameters);
+
+		tempReport = Files.createTempFile(tempDir, "test_",".elib").toFile();
+		tempReport.delete();
 	}
 
 	@After
@@ -107,6 +113,10 @@ public class EndToEndIT {
 			FileUtils.deleteQuietly(fastaFile);
 			fastaFile = null;
 		}
+		if (null != tempReport){
+			FileUtils.deleteQuietly(tempReport);
+			tempReport = null;
+		}
 		if (null != tempDir) {
 			FileUtils.deleteDirectory(tempDir.toFile());
 			tempDir = null;
@@ -124,10 +134,6 @@ public class EndToEndIT {
 		outputFile.openFile(FileUtils.getFile(tempDir.toFile(),diaFile.getName() + ".elib"));
 
 		assertSanityTest(outputFile,400,300);
-
-		File tempReport = Files.createTempFile(tempDir, "test_",".elib").toFile();
-
-		tempReport.delete();
 		SearchToBLIB.convert(new EmptyProgressIndicator(), ImmutableList.of(jobDataA),tempReport,false,true);
 		assertTrue(FileUtils.directoryContains(tempDir.toFile(),tempReport));
 
@@ -163,9 +169,6 @@ public class EndToEndIT {
 
 		assertSanityTest(outputFile,400,400);
 
-		File tempReport = Files.createTempFile(tempDir, "test_",".elib").toFile();
-
-		tempReport.delete();
 		SearchToBLIB.convert(new EmptyProgressIndicator(), ImmutableList.of(jobDataA,jobDataB,jobDataC),tempReport,false,false);
 		assertTrue(FileUtils.directoryContains(tempDir.toFile(),tempReport));
 
@@ -187,10 +190,6 @@ public class EndToEndIT {
 
 		//the output assertions here would be the same as for testWholePipelineMultipleData, no sense
 		//checking them twice
-
-		File tempReport = Files.createTempFile(tempDir, "test_",".elib").toFile();
-
-		tempReport.delete();
 		SearchToBLIB.convert(new EmptyProgressIndicator(), ImmutableList.of(jobDataA,jobDataB,jobDataC),tempReport,false,true);
 		assertTrue(FileUtils.directoryContains(tempDir.toFile(),tempReport));
 
