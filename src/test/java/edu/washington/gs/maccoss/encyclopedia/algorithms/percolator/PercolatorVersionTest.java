6c2eb0ac1f87e985a6730010ae95b7004bb343b3
Seth Just <sethjust@sethjust.com>
2022-05-09 16:07:27 -0700

    Add test for parsing Percolator version arguments.

---
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/percolator/PercolatorVersionTest.java
@@ -0,0 +1,17 @@
+package edu.washington.gs.maccoss.encyclopedia.algorithms.percolator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PercolatorVersionTest {
+	@Test
+	public void testParsePercolator() throws Exception {
+		assertEquals(PercolatorVersion.v2p10, PercolatorVersion.getVersion(PercolatorVersion.V2_10));
+		assertEquals(PercolatorVersion.v3p01, PercolatorVersion.getVersion(PercolatorVersion.V3_01));
+		assertEquals(PercolatorVersion.v3p05, PercolatorVersion.getVersion(PercolatorVersion.V3_05));
+		assertEquals(PercolatorVersion.v3p05, PercolatorVersion.getVersion("3"));
+		assertEquals(PercolatorVersion.v3p05, PercolatorVersion.getVersion("3.5"));
+		assertEquals(PercolatorVersion.v3p01, PercolatorVersion.getVersion("3.1"));
+	}
+}
