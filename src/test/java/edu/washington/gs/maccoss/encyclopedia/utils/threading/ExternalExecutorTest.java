f86bfc84dfa44eb52b96a91a032e560a549afb4f
Seth Just <sethjust@sethjust.com>
2022-05-09 15:59:04 -0700

    Add a unit test for a buggy use case of ExternalExecutor.

--- src/test/java/edu/washington/gs/maccoss/encyclopedia/utils/threading/ExternalExecutorTest.java
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/utils/threading/ExternalExecutorTest.java
@@ -1,8 +1,16 @@
 package edu.washington.gs.maccoss.encyclopedia.utils.threading;
 
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 
+import edu.washington.gs.maccoss.encyclopedia.utils.OSDetector;
 import edu.washington.gs.maccoss.encyclopedia.utils.io.OutputMessage;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsNot;
+import org.junit.Assume;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ExternalExecutorTest {
 	public static void main(String[] args) throws Exception {
@@ -19,5 +27,51 @@ public class ExternalExecutorTest {
 		}
 		System.out.println(count+", "+result.size());
 		System.out.println("FINISHED!");
+	}
+
+	@Test
+	public void testExternalOutputRaceCondition() throws Exception {
+		// In code prior to 2.0.0-SNAPSHOT there's a potential race condition if the
+		// output queue is ever drained entirely after the process has exited, even
+		// if there's still unprocessed output from the process! This can conceivably
+		// happen to any process, but realistically will only affect very short-lived
+		// processes, or output that's written very close to when the process exits.
+		//
+		// The root cause of this bug is that ExternalExecutor#isFinished() returns
+		// true if the process has exited but the output-handling threads are still
+		// running!
+
+		// Skip the test on Windows
+		Assume.assumeThat(OSDetector.getOS(), new IsNot<>(new IsEqual<>(OSDetector.OS.WINDOWS)));
+
+		// Build a random test string
+		final int n = new Random().nextInt(100);
+		final StringBuilder sb = new StringBuilder();
+		for (int i = n; i --> 0;) {
+			sb.append(i);
+			if (i > 0) {
+				sb.append("\n");
+			}
+		}
+
+		final ExternalExecutor e = new ExternalExecutor(new String[] {"echo", sb.toString()});
+
+		final BlockingQueue<?> q = e.start();
+
+		int count = 0;
+
+		// If we uncomment this line this test case almost always succeeds, as there's enough
+		// time for at least one message to be enqueued.
+//		Thread.sleep(1);
+
+		// Match existing usages of ExternalExecutor -- loop until finished and no output
+		while (!e.isFinished() || !q.isEmpty()) {
+			if (!q.isEmpty()) {
+				q.take();
+				count++;
+			}
+		}
+
+		assertEquals(n, count);
 	}
 }
