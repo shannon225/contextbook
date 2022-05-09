d9b5ac3b31f377fcd7fc2bd02d4a3b862691519e
Seth Just <sethjust@sethjust.com>
2022-05-09 15:13:51 -0700

    Make PercolatorVersion an interface.

--- src/main/java/edu/washington/gs/maccoss/encyclopedia/algorithms/percolator/PercolatorVersion.java
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/algorithms/percolator/PercolatorVersion.java
@@ -1,16 +1,20 @@
 package edu.washington.gs.maccoss.encyclopedia.algorithms.percolator;
 
-public enum PercolatorVersion {
-	v2p10,v3p01, v3p05;
-	
-	public static final String V3_05="v3-05";
-	public static final String V3_01="v3-01";
-	public static final String V2_10="v2-10";
-	public static final PercolatorVersion DEFAULT_VERSION=PercolatorVersion.v3p01;
-	public static final PercolatorVersion[] VALID_VERSIONS=new PercolatorVersion[] {v3p01, v2p10};
+public interface PercolatorVersion {
+	PercolatorVersion v2p10 = InternalPercolatorVersion.v2p10;
+	PercolatorVersion v3p01 = InternalPercolatorVersion.v2p10;
+	PercolatorVersion v3p05 = InternalPercolatorVersion.v2p10;
 
-	public static PercolatorVersion getVersion(String s) {
-		if (s==null||s.length()==0) return v3p05;
+	PercolatorVersion DEFAULT_VERSION = PercolatorVersion.v3p01;
+
+	PercolatorVersion[] VALID_VERSIONS = new PercolatorVersion[]{v3p01, v2p10};
+
+	String V3_05 = "v3-05";
+	String V3_01 = "v3-01";
+	String V2_10 = "v2-10";
+
+	static PercolatorVersion getVersion(String s) {
+		if (s == null || s.length() == 0) return v3p05;
 		if (V2_10.equals(s)) return v2p10;
 		if (V3_01.equals(s)) return v3p01;
 		if (V3_05.equals(s)) return v3p05;
@@ -23,31 +27,36 @@ public enum PercolatorVersion {
 		if ("3.05".equals(s)) return v3p05;
 		return v3p05;
 	}
-	
-	@Override
-	public String toString() {
-		switch (this) {
-		case v2p10:
-			return V2_10;
-		case v3p01:
-			return V3_01;
-		case v3p05:
-			return V3_05;
-		default:
-			return DEFAULT_VERSION.toString();
+
+	int getMajorVersion();
+
+	enum InternalPercolatorVersion implements PercolatorVersion {
+		v2p10, v3p01, v3p05;
+
+		@Override
+		public String toString() {
+			switch (this) {
+				case v2p10:
+					return V2_10;
+				case v3p01:
+					return V3_01;
+				case v3p05:
+					return V3_05;
+				default:
+					return DEFAULT_VERSION.toString();
+			}
 		}
-	}
-	
-	public int getMajorVersion() {
-		switch (this) {
-		case v2p10:
-			return 2;
-		case v3p01:
-			return 3;
-		case v3p05:
-			return 3;
-		default:
-			return DEFAULT_VERSION.getMajorVersion();
+
+		@Override
+		public int getMajorVersion() {
+			switch (this) {
+				case v2p10:
+					return 2;
+				case v3p01:
+				case v3p05:
+				default:
+					return 3;
+			}
 		}
 	}
 }
