c7dfefa784d8215cd8505bb0906e667958674288
Seth Just <sethjust@sethjust.com>
2022-08-04 11:06:35 -0700

    Add utility method for argument parsing.

--- src/main/java/edu/washington/gs/maccoss/encyclopedia/utils/CommandLineParser.java
+++ src/main/java/edu/washington/gs/maccoss/encyclopedia/utils/CommandLineParser.java
@@ -1,10 +1,13 @@
 package edu.washington.gs.maccoss.encyclopedia.utils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class CommandLineParser {
-	
 	public static HashMap<String, String> parseArguments(String[] args) {
 		HashMap<String, String> map=new HashMap<String, String>();
 		if (args.length==0) {
@@ -38,4 +41,35 @@ public class CommandLineParser {
 		return args.toArray(new String[0]);
 	}
 
+	/**
+	 * Parse the given command line arguments into a list of paths specified by {@code -i}
+	 * and the remaining parameters wtih {@link #parseArguments(String[])}.
+	 */
+	public static Pair<List<Path>, HashMap<String, String>> parseFilesAndArguments(String[] args) {
+		return parseFilesAndArguments(args, "-i");
+	}
+
+	/**
+	 * Parse the given command line arguments into a list of paths specified by {@code filesFlag}
+	 * and the remaining parameters wtih {@link #parseArguments(String[])}.
+	 */
+	public static Pair<List<Path>, HashMap<String, String>> parseFilesAndArguments(String[] args, String filesFlag) {
+		final List<String> mutableArgs = Lists.newLinkedList(Arrays.asList(args));
+		final List<Path> paths = Lists.newArrayListWithExpectedSize(1);
+
+		for (Iterator<String> iter = mutableArgs.iterator(); iter.hasNext();) {
+			if (Objects.equals(filesFlag, iter.next())) {
+				iter.remove(); // pop the flag
+
+				paths.add(Paths.get(iter.next()));
+
+				iter.remove(); // pop the path
+			}
+		}
+
+		return new Pair<>(
+				ImmutableList.copyOf(paths),
+				parseArguments(mutableArgs.toArray(new String[0]))
+		);
+	}
 }
