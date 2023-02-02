0a5c3cb91e296c8f715840973c3e5715e4c7fd49
Brian Searle <searleb@Studio.local>
2023-02-02 18:29:29 -0500

    edits to test

--- src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/curve/DilutionCurveFitterExample.java
+++ src/test/java/edu/washington/gs/maccoss/encyclopedia/algorithms/curve/DilutionCurveFitterExample.java
@@ -21,13 +21,13 @@ public class DilutionCurveFitterExample {
 
 	
 	public static void main(String[] args) throws Exception {
-		final File outputDirectory=new File("/Users/searleb/Downloads/drive-download-20230110T003145Z-001/LOD_LOQ_yi_sampleprep_titration/curve_fitting");
+		final File outputDirectory=new File("/Users/searleb/Downloads/LOD_LOQ_yi_sampleprep_titration_updated/curve_fitting");
 		final File targetDirectory=new File(outputDirectory, "target");
 		outputDirectory.mkdirs();
 		targetDirectory.mkdirs();
 		
-		File dataFile=new File("/Users/searleb/Downloads/drive-download-20230110T003145Z-001/LOD_LOQ_yi_sampleprep_titration/2022_09_26_cell_titration_exp_quant.elib.peptides.txt");
-		File sampleOrganizationFile=new File("/Users/searleb/Downloads/drive-download-20230110T003145Z-001/LOD_LOQ_yi_sampleprep_titration/sample_organization_yi.tsv");
+		File dataFile=new File("/Users/searleb/Downloads/LOD_LOQ_yi_sampleprep_titration_updated/2022_09_26_cell_titration_exp_quant.elib.peptides.txt");
+		File sampleOrganizationFile=new File("/Users/searleb/Downloads/LOD_LOQ_yi_sampleprep_titration_updated/prm_sample_organization_tcell_sample_prep.csv");
 
 		
 		Pair<ArrayList<ScoredObject<String>>, Map<String, TObjectFloatHashMap<String>>> concentrationPair=DilutionCurveFitter.getExpectedConcentrationsFromCSV(sampleOrganizationFile);
