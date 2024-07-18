f64ec5f9025bc5428e2bed6409a13bff2153f430
Brian C. Searle <searleb@gmail.com>
2024-07-17 22:02:51 -0400

    Update to version for release encyclopedia-master-4.7.17.

--- increment_version.sh
+++ increment_version.sh
@@ -7,22 +7,28 @@ program="encyclopedia";
 #minor=`awk '/<version>/ {split($1, a, "."); split(a[3], b, "-"); print b[1]; exit 1}' pom.xml`; 
 #mp1=$((minor+1));
 
-if [ "$#" -ne 2 ]; then
-	echo "You must specify a current and a target version (e.g. \"increment_version.sh 0.4.7 0.4.8\")";
-	exit 1;
-fi
+#if [ "$#" -ne 2 ]; then
+#	echo "You must specify a current and a target version (e.g. \"increment_version.sh 0.4.7 0.4.8\")";
+#	exit 1;
+#fi
 
-echo "Updating ${program} from [${1}] to [${2}]";
+branch=$(git symbolic-ref --short HEAD);
+let "year=`date +%Y`-2020";
+current=${year}.`date +%-m.%-d`;
+let "next=${year}+1";
+next=${next}.0.0;
+
+echo "Updating ${program} $branch from [${current}] to [${next}]";
 echo `find . -name '*.java' -exec wc {} \; | awk '{sum=sum+$1} END {print sum}'` "total lines of Java code"
 
-TAG=${program}-${1}
+TAG=${program}-${branch}-${current}
 
-mvn versions:set -DnewVersion="${1}"
-git commit -am "Update to version for release ${program}-${1}."
+mvn versions:set -DnewVersion="${current}"
+git commit -am "Update to version for release ${program}-${branch}-${current}."
 git tag "${TAG}" # optional
 mvn clean package -DbuildJars;
-mvn versions:set -DnewVersion="${2}-SNAPSHOT"
+mvn versions:set -DnewVersion="${next}-${branch}-SNAPSHOT"
 git commit -am "Update to next SNAPSHOT version."
 
-echo "Finished updating from [${1}] to [${2}]";
+echo "Finished updating from [${current}] to [${next}]";
 echo "Remember to run \`git push origin --tags\` or \`git push origin ${TAG}\`!"
